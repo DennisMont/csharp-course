3f54d33fb577fc530702163790460e58d85e080d
Dennis Montano <dmmv429@gmail.com>
2025-05-26 13:58:57 -0400

    Data structures homework 2

--- DataStructures/DataStructures/CustomQueue.cs
+++ DataStructures/DataStructures/CustomQueue.cs
@@ -1,35 +1,81 @@
-﻿namespace DataStructures
+﻿using System.Collections;
+
+namespace DataStructures
 {
     // FIFO
     public class CustomQueue<T>
     {
-        // TODO: Implement queue (you can modify the implementation, not the names of the methods)
-        private int _count = 0;
+        private class Node
+        {
+            public T Value { get; set; }
+            public Node? Next { get; set; } = null!;
 
+            public Node(T value)
+            {
+                Value = value;
+            }
+        }
+
+        private Node? _head;
+        private Node? _tail;
         public int Count => _count;
+        private int _count = 0;
 
         public void Enqueue(T item)
         {
-            // Add items
+            var newNode = new Node(item);
+
+            if (_head == null)
+            {
+                _head = _tail = newNode;
+            }
+            else
+            {
+                _tail!.Next = newNode;
+                _tail = newNode;
+            }
+            
+            _count++;
         }
 
         public T Dequeue()
         {
-            // remove items
-            return default!;
+            if (_head == null)
+            {
+                throw new InvalidOperationException("Queue is empty.");
+            }
+
+            T FirstItem = _head.Value;
+            _head = _head.Next;
+            _count--;
+
+            return FirstItem;
         }
 
         public T Peek()
         {
-            // remove items
-            return default!;
+            if (_head == null)
+            {
+                throw new InvalidOperationException("Stack is empty.");
+            }
+
+            return _head.Value;
         }
 
-        public bool IsEmpty() => true;
+        public bool IsEmpty() => _count == 0;
 
         public T[] ToArray()
         {
-            return default!;
+            T[] array = new T[_count];
+            int indexArray = 0;
+
+            while (!IsEmpty())
+            {
+                array[indexArray] = Dequeue();
+                indexArray++;
+            }
+
+            return array;
         }
     }
 }
